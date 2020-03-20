460d7a81c313c366f98f00079eae763f19181e1c
chamodlakmal <chamodlakmal97@gmail.com>
2020-03-20 21:04:45 +0530

    created angular project and completed feature1

--- online-book-store/src/main/java/com/chamiviews/online/book/store/repository/BookRepository.java
+++ online-book-store/src/main/java/com/chamiviews/online/book/store/repository/BookRepository.java
@@ -1,11 +1,11 @@
 package com.chamiviews.online.book.store.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.chamiviews.online.book.store.entity.Book;
 
-
+@CrossOrigin("*")
 public interface BookRepository extends JpaRepository<Book, Long>{
 	
 }
