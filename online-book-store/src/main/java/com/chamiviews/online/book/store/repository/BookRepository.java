59b133d16b0a1c950f3e9546449dbbaf80eb277f
chamodlakmal <chamodlakmal97@gmail.com>
2020-03-21 14:02:10 +0530

    completed feature 3

--- online-book-store/src/main/java/com/chamiviews/online/book/store/repository/BookRepository.java
+++ online-book-store/src/main/java/com/chamiviews/online/book/store/repository/BookRepository.java
@@ -1,11 +1,17 @@
 package com.chamiviews.online.book.store.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.chamiviews.online.book.store.entity.Book;
 
 @CrossOrigin("*")
 public interface BookRepository extends JpaRepository<Book, Long>{
+	@RestResource(path = "categoryid")
+	Page<Book> findByCategoryId(@Param("id") Long id,Pageable pageable);
 	
 }
